cefd7920e1541e54fba7281c79b646a581b87a1c
Alex <Alex@HiveTech.ai>
2026-07-24 09:34:15 -0600

    fix: Stop Firefox default browser prompt

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1043,6 +1043,7 @@ in
       zsh.enable = true;
       firefox = {
         enable = true;
+        policies.DontCheckDefaultBrowser = true;
         policies.ExtensionSettings."{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
           installation_mode = "normal_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
