2b7885d46e708b9a431adc8400c06e2ee78e3f26
Alex <Alex@HiveTech.ai>
2026-08-21 07:28:45 -0600

    fix: use host Nix on Ubuntu WSL

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -1,8 +1,18 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   toolsets = import ../../lib/toolsets.nix { inherit lib pkgs; };
+  packages =
+    if config.dotfiles.profile == "ubuntu-wsl" then
+      lib.remove pkgs.nix toolsets.development
+    else
+      toolsets.development;
 in
 {
   imports = [ ./core.nix ];
-  config.home.packages = toolsets.development;
+  config.home.packages = packages;
 }
